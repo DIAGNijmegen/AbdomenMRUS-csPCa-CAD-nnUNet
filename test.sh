9595835ba9078ef9e3bcbd1f999fbb89171c3b87
Joeran Bosma <joeran@bosma.co>
2023-03-11 11:45:54 +0100

    Cleanup

--- test.sh
+++ test.sh
@@ -4,17 +4,17 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 ./build.sh
 
-VOLUME_SUFFIX=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+VOLUME_SUFFIX=$(dd if=/dev/urandom bs=32 count=1 | md5sum | cut -c 1-10)
 
-DOCKER_FILE_SHARE=prostatecancerdetectioncontainer-output-$VOLUME_SUFFIX
+DOCKER_FILE_SHARE=prostate_cancer_detection_processor-output-$VOLUME_SUFFIX
 docker volume create $DOCKER_FILE_SHARE
 # you can see your output (to debug what's going on) by specifying a path instead:
 # DOCKER_FILE_SHARE="/mnt/netcache/pelvis/projects/joeran/tmp-docker-volume"
 
-docker run --rm \
+docker run --cpus=4 --memory=32gb --shm-size=32gb --gpus='"device=0"' --rm \
         -v $SCRIPTPATH/test/:/input/ \
         -v $DOCKER_FILE_SHARE:/output/ \
-        prostatecancerdetectioncontainer
+        joeranbosma/prostate_cancer_detection_processor
 
 docker run --rm \
         -v $DOCKER_FILE_SHARE:/output/ \
